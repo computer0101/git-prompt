9bde669ad14f0436b4f1310b26a9ae5bfdb16c06
Devesh Patel <deveshpatel7643@gmail.com>
2018-07-29 15:25:10 +0530

    change color of username and modify ending character

--- git-prompt.sh
+++ git-prompt.sh
@@ -10,7 +10,7 @@ then
 	. ~/.config/git/git-prompt.sh
 else
 	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
-	PS1="$PS1"'\[\033[40m\]'       # change to green
+	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u'             # user@host<space>
 	PS1="$PS1"'\[\033[40m\]'       # change to brownish yellow
 	PS1="$PS1"'\w'                 # current working directory
@@ -29,7 +29,7 @@ else
 		fi
 	fi
 	PS1="$PS1"'\[\033[0m\]'        # change color
-	PS1="$PS1"' $ '                 # prompt: always $
+	PS1="$PS1"' ► '                 # prompt: always $
 fi
 
 MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
